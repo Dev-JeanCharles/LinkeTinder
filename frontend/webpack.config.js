d945216c156487e956b03b4c2961c814455c38a4
Dev-JeanCharles <jeancduasan@gmail.com>
2024-03-15 15:13:02 -0300

    start: frontend

---
+++ frontend/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: ['./src/index.ts'],
+        cadastroCandidato: './src/Controller/cadastroCandidato.ts',
+        cadastroEmpresa: './src/Controller/cadastroEmpresa.ts',
+        perfilCandidato: './src/Controller/perfilCandidato.ts',
+        perfilEmpresa: './src/Controller/perfilEmpresa.ts',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'public', 'dist'),
+    },
+};
