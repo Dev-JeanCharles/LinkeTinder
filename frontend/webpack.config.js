51aef1b8e341ec1fb88d02bc79a2f8a02902c0aa
Dev-JeanCharles <jeancduasan@gmail.com>
2024-03-15 22:33:59 -0300

    feat: html e css desenvolvido

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,12 +3,30 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     entry: {
-        index: ['./src/index.ts'],
         cadastroCandidato: './src/Controller/cadastroCandidato.ts',
         cadastroEmpresa: './src/Controller/cadastroEmpresa.ts',
         perfilCandidato: './src/Controller/perfilCandidato.ts',
         perfilEmpresa: './src/Controller/perfilEmpresa.ts',
     },
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        port: 8080,
+        hot: true,
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'public', 'dist'),
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+            { from: 'public' },
+            ]
+        })
+        ],
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
     module: {
         rules: [
             {
@@ -17,12 +35,5 @@ module.exports = {
                 exclude: /node_modules/,
             },
         ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'public', 'dist'),
     },
 };
