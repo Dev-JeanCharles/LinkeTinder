25f5493cb609a12be84c8226cbb2234c9a093eda
Dev-JeanCharles <jeancduasan@gmail.com>
2024-04-22 18:48:49 -0300

    refactor: aplicando principios de clean code

--- backend/src/test/groovy/CandidateDAOTest.groovy
+++ backend/src/test/groovy/CandidateDAOTest.groovy
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import static org.junit.jupiter.api.Assertions.*;
 
-class RegisterTest {
+class CandidateDAOTest {
 
 //    static Register register
 //
