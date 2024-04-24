d7b272118d020f83342ea753903f03d858635e7e
Dev-JeanCharles <jeancduasan@gmail.com>
2024-04-24 19:33:52 -0300

    refactor: aplicando principios do S.O.L.I.D

---
+++ backend/src/main/groovy/org/jean/linketinder/Interfaces/DB/DatabaseConnection.groovy
@@ -0,0 +1,29 @@
+package org.jean.linketinder.Interfaces.DB
+
+import java.sql.Connection
+import java.sql.DriverManager
+
+interface DatabaseConnection {
+    Connection connect()
+}
+
+class DBConnection implements  DatabaseConnection {
+
+    static Connection connection = null
+
+    @Override
+    Connection connect() {
+        String dbname = "linkertinder"
+        String user = "postgres"
+        String password = "123"
+
+        try {
+            Class.forName("org.postgresql.Driver")
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/$dbname", user, password)
+
+        } catch (Exception e) {
+            e.printStackTrace()
+        }
+        return connection
+    }
+}
